2ac381bc824fcce03a5e50f4f078d53fadf0a129
Akshat Jain <67745418+akshatprogrammer@users.noreply.github.com>
2021-01-12 10:50:09 +0530

    Create day1_2.cpp

---
+++ day1_2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<malloc>        // std lib to include malloc
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;    // taking size of array
+  int *a;
+  a = (int *)malloc(sizeof(int)*n);    // allocating size to array dynamically
+  for(int i=0;i<n;i++)
+  {
+    cin >> a[i];    // inputting array.
+  }
+  for(int i=0;i<n/2;i++)
+  {
+    a[n-i-1] = a[i];       // swapping elements 
+  }
+  for(int i=0;i<n;i++)
+  {
+    cout << a[i];    // printing array.
+  }
+}
+/*
+Time Complexity = O(n*n/2)
+Space Complexity = O(1)
+By : Akshat Jain
+*/
